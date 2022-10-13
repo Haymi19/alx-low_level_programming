95d9a3532359683e38e0d8445fe5817d4514019d
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-13 13:28:48 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+using namespace std;
+int main()
+{
+        //string initialization
+    char Mystr[] = ""programming is like building a multilingual puzzle"
+
+    puts(Mystr); //writing the string to stdout
+
+    return 0;
+}
