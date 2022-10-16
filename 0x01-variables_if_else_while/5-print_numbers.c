bd30cdee3cf7e1e9dbeadb5a5341275c0271a103
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-16 08:31:52 -0700

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0 to 9
+ *
+ * Return: Alway 0 (Success)
+ *
+ * Betty: Documentation style
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+	{
+		putchar(i);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
