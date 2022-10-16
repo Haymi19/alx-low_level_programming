6b6d8fa08993a159db51bb8b60baecb215bbae35
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-16 08:45:30 -0700

    single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -print single digit numbers
+ *
+ * Return: Always 0 (Success)
+ *
+ * Betty: Documentation style
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
