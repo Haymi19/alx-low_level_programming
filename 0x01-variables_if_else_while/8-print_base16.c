e8155d9527295bad76e0cc68b72049513e633bc0
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-17 05:08:10 -0700

    base16 hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,13 @@
  */
 int main(void)
 {
+	char base16[16] = "0123456789abcdef";
+
 	int i;
 
 	for (i = 0; i < 16; i++;)
 	{
-		putchar("%x", i);
+		putchar("%x", base16[i]);
 	}
 	putchar('\n');
 
