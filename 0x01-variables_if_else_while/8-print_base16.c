8b3c692ad7bd745ac69826b2cef9865734df2130
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-17 05:40:09 -0700

    base16 to hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,18 +10,12 @@ int main(void)
 {
 	char base16[16] = "0123456789abcdef";
 
-	char hex[i] = "%x";
-
 	int i;
 
 	for (i = 0; i < 16; i++)
 	{
 		putchar(base16[i]);
 
-		while (base16[i] = hex[i];
-		{
-		putchar(hex[i]);
-		}
 	}
 	putchar('\n');
 
