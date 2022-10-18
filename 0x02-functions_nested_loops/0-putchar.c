83806a138b1b69ac23d75626d0751b1ab1b6b15f
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-18 08:56:18 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * main - "_putchar"
+ *
+ * Return: Always 0 (Success)
+ *
+ * Betty: Documentation style
+ */
+int main(void)
+{
+	int i;
+
+	char c = _putchar;
+
+	for
+	       i = c;
+
+	{
+
+	putchar(c, i);
+
+	}
+	putchar('\n');
+
+	return (0);
+
+}
+
