ad04265e2d354fecb1020ab04b8ca5f4f2c37197
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-18 09:44:42 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ *
+ * Betty: Documentation style
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	int i;
+
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz"
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
