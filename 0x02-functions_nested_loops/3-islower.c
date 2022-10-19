05f562f2c89d1a84e502099b339d04461e1428d8
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-19 02:58:19 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print alphabet islower.
+ *
+ * Return 1 if c islowercase
+ * Return 0 otherwise
+ */
+int _islower(int c)
+{
+	int c;
+	for (c = 97; c < 122; c++)
+	{
+		_putchar(c);
+		return (1);
+	}
+	else
+		_putchar(c);
+	return(0);
+}
