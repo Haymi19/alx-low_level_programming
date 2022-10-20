9ca78ab2bdbf54da21ed16d39cc843e86e7c2e2a
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-20 15:45:19 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,18 +12,15 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size > 0)
+	for (i = 1; i < size; i++)
 	{
-		for (i = 1; i <= size; i++)
-		{
-			for (j = 1; j <= i; j++)
-			{
-				_putchar(35);
-			}
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
 			_putchar('\n');
-		}
 	}
-	else
-		_putchar('\n');
+	_putchar('\n');
 }
 
