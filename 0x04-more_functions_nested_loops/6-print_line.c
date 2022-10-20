10b07eff5a60ecc9f463178e2f19cc980eebf27d
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-20 15:22:12 -0700

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,13 +10,13 @@
  */
 void print_line(int n)
 {
-	if (n > 0)
+	int i;
+
+	for (i = 0; i < n;  i++)
 	{
 		_putchar(95);
-		_putchar('\n');
-	}
-	else
-		_putchar('\n');
 
+	}
+	 _putchar('\n');
 }
 
