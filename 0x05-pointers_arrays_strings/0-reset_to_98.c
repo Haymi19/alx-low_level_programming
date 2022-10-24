8b2ff6d62883971e403253f55cb9a49ddecdc41e
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-24 13:09:15 -0700

    Update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,23 +3,11 @@
 /**
  * reset_to_98 - Update the value of n.
  *
- * @p: an address of the parameter n.
+ * @n: value of an integer.
  *
  * Return: Value of n.
  */
 void reset_to_98(int *n)
 {
-	int *p;
-
-	n = 402;
-
-	p = &n;
-
-	_putchar('n');
-
-	*p = 98;
-
-	_putchar('n');
-
-	_putchar('\n');
+	*n = 98;
 }
