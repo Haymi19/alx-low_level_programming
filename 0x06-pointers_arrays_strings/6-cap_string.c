fae5a2c73389a9bb2eb6874959bade73bebc26eb
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-27 11:33:24 -0700

    letters to uppercae

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,12 +10,13 @@
  */
 char *cap_string(char *s)
 {
-        int counter = 0, i;
+	int counter = 0, i;
 
 	int sep_words[] = {9, 32, 33, 44, 46, 59, 40, 41, 63, 123, 125};
 
 	if ((*(s + counter) >= 97) && (*(s + counter) <= 122))
 		*(s + counter) = *(s + counter) - 32;
+
 	counter++;
 
 	while (*(s + counter) != '\0')
@@ -29,9 +30,8 @@ char *cap_string(char *s)
 				break;
 			}
 		}
-
-                counter++;
-        }
-        return (s);
+		counter++;
+	}
+	return (s);
 }
 
