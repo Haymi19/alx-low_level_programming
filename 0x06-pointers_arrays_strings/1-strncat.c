52fd06618f39a38205094b5726038f5cef69b797
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-10-27 13:11:23 -0700

    n concatenate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,33 +6,37 @@
  *
  * @src: string one.
  * @dest : string two.
+ * @n: number of bytes.
  *
  * Return: the pointer to dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-        int count = 0, count1 = 0;
+	int count = 0, count1 = 0;
 
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
-        	while (*(dest + count) != '\0')
-        	{
-                	count++;
-        	}
+		while (*(dest + count) != '\0')
+		{
+			count++;
+		}
 
-        	while (count1 >= 0)
-        	{
-                	*(dest + count) = *(src + count1);
-                	if (*(src + count1) == '\0')
-                        	break;
+		while (count1 >= 0)
+		{
+			*(dest + count) = *(src + count1);
+			if (*(src + count1) == '\0')
+				break;
 
-                	count++;
-                	count1++;
-        	}
+			count++;
+			count1++;
+		}
+	if (i >= n)
+		*(src + count1) != '\0';
+		count1++;
 	}
 
-        return (dest);
+	return (dest);
 }
 
