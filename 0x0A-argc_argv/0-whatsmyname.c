0b64caca2529e8d5425123d8df8dbc1c0b3972ad
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-11-04 08:35:44 -0700

    program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - prints its name, followed by a new line.
  *
  * @argc: counts the arguments.
- * @argv[]: pointer to the array of arguments.
+ * @argv: pointer to the array of arguments.
  *
  * Return: Always 0 (Success)
  */
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
-		printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 	}
 
 	return (0);
