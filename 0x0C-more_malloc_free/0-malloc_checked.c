6862257e16e83e638b43e06121bd58cffefd22ea
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-11-10 09:59:02 -0800

    nconcat

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc.
+ *
+ * @b: input integer.
+ *
+ * Return: a pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	unsigned int size = 0, i;
+
+	for (i = 0; i < size; i++)
+	{
+		ptr = malloc(size * sizeof(b));
+
+		if (ptr == NULL)
+		{
+			free(ptr);
+			return (0);
+		}
+		return(ptr);
+	}
+	return (0);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef _main_h_
+#define _main_h_
+
+int _putchar(char);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+#endif
