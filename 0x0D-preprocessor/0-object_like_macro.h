3b73b557e4eb1ab8e5c3264cd8ebbe7098c73c12
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-11-14 00:59:52 -0800

    preproccessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef _main_h_
+#define _main_h_
+#define SIZE 1024
+
+int _putchar(char);
+
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,7 @@
+#ifndef _main_h_
+#define _main_h_
+#define PI 3.14159265359
+
+int _putchar(char);
+
+#endif
