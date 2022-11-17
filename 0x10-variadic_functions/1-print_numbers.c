ef28d4d4a06ed7ec03f83f23f98d0bcb7e1f7e4a
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-11-17 12:41:34 -0800

    variadic_functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,22 +14,20 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list args;
+
 	unsigned int i;
 
-	if (separator != NULL)
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
 	{
-		va_list args;
+		printf("%d", va_arg(args, const unsigned int));
 
-		va_start(args, n);
-
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(args, const unsigned int));
-
-			if (i != (n - 1))
-				printf("%s", separator);
-		}
-		va_end(args);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
+	va_end(args);
+
 	printf("\n");
 }
