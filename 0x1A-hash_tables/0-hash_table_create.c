74c5c0d37302377ac3be3914d7f7be061f8ca97e
Haimanot Eshete <haimanoteshete685@gmail.com>
2023-02-03 10:01:43 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = (hash_table_t *) malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
-                return (NULL);
+		return (NULL);
 
 	table->size = size;
 
