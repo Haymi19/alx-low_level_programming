d564bd301d1b7d58d8ae77a0ce0c51ef033dcfbc
Haimanot Eshete <haimanoteshete685@gmail.com>
2023-05-10 12:28:49 -0700

    exponential search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -46,5 +46,7 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+int binary_search_helper(int *array, int value, size_t low, size_t high);
+size_t min(size_t a, size_t b);
 
 #endif
